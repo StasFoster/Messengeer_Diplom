3cae2ac014a91e09cf4c5d7e27bd544576933ac9
StasFoster <stanislav06052005@gmail.com>
2026-03-08 02:03:36 +0400

    Добавил стили и разметку

--- MessengerProject/Chats/views.py
+++ MessengerProject/Chats/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def main(request):
-    return HttpResponse("Test")
+    return render(request, "Chats/index.html")
